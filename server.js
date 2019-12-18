3251a8071b6a137ce6a106ed6320c2f7601b0e91
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-18 14:51:04 -0500

    added movie posters to server

--- server.js
+++ server.js
@@ -13,42 +13,48 @@ let movies = [
 		title: "The Godfather",
 		director: "Francis Ford Coppola",
 		metascore: 100,
-		stars: ["Marlon Brando", "Al Pacino", "Robert Duvall"]
+		stars: ["Marlon Brando", "Al Pacino", "Robert Duvall"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/6326/p6326_v_v8_aj.jpg'
 	},
 	{
 		id: 1,
 		title: "Star Wars",
 		director: "George Lucas",
 		metascore: 92,
-		stars: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"]
+		stars: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/4407/p4407_v_v8_be.jpg'
 	},
 	{
 		id: 2,
 		title: "The Lord of the Rings: The Fellowship of the Ring",
 		director: "Peter Jackson",
 		metascore: 92,
-		stars: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"]
+		stars: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/28828/p28828_v_v8_bg.jpg'
 	},
 	{
 		id: 3,
 		title: "Terminator 2: Judgement Day",
 		director: "James Cameron",
 		metascore: 94,
-		stars: ["Arnold Schwarzenegger", "Edward Furlong", "Linda Hamilton"]
+		stars: ["Arnold Schwarzenegger", "Edward Furlong", "Linda Hamilton"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/13308/p13308_v_v8_al.jpg'
 	},
 	{
 		id: 4,
 		title: "Dumb and Dumber",
-		director: "The Farely Brothers",
+		director: "The Farrelly Brothers",
 		metascore: 76,
-		stars: ["Jim Carrey", "Jeff Daniels", "Lauren Holly"]
+		stars: ["Jim Carrey", "Jeff Daniels", "Lauren Holly"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/16298/p16298_v_v8_aa.jpg'
 	},
 	{
 		id: 5,
 		title: "Tombstone",
 		director: "George P. Cosmatos",
 		metascore: 89,
-		stars: ["Kurt Russell", "Bill Paxton", "Sam Elliot"]
+		stars: ["Kurt Russell", "Bill Paxton", "Sam Elliot"],
+		poster: 'http://www.gstatic.com/tv/thumb/v22vodart/15292/p15292_v_v8_ap.jpg'
 	}
 ];
 
@@ -81,7 +87,8 @@ app.put("/api/movies/:id", (req, res) => {
 		!req.body.title ||
 		!req.body.director ||
 		!req.body.metascore ||
-		!req.body.stars
+		!req.body.stars ||
+		!req.body.poster
 	) {
 		res
 		.status(422)
